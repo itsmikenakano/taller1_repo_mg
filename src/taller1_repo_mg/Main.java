8f21280adb011ecc77a5871d37869928a42df2cc
ASUS <ASUS@ASUS-GAMMER>
2022-05-17 19:53:31 -0500

    Archivos necesarios apra el proyecto

---
+++ src/taller1_repo_mg/Main.java
@@ -0,0 +1,10 @@
+package taller1_repo_mg;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer repositorio");
+	}
+
+}
